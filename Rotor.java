b9ffcd6ba64c7883d5e4b68859a4bb617d48dc6c
Jorge-Garcia05 <146406914+Jorge-Garcia05@users.noreply.github.com>
2025-09-29 07:22:19 -0400

    implemented rotor

--- Rotor.java
+++ Rotor.java
@@ -7,13 +7,44 @@ public class Rotor {
     public Rotor(String v, char c){
         this.rotorValues = new String(v);
         this.startChar = c;
+
+        int pos = rotorValues.indexof(startChar);
+        while (rotorValues.charAt(0) != startChar) {
+            rotate();
+        }
+    
+
         
-        while(!this.rotate());
             
     }
     
     public boolean rotate(){
-        //TODO
+
+        
+
+        //shoud create array of rotor characters that should be the size of 27
+        char[] arr = rotorValues.toCharArray();
+        //set the temp to last index of array 
+        char temp = arr[26];
+        //for loop to shift elements to right
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];      
+        }
+        //set the last element to be the first index now
+        arr[0] = temp;                
+        //set the new rotated string to be the next string to rotate
+        rotorValues = new String(arr);
+
+        //return true when makes a full cycle
+
+
+        return rotorValues.charAt(0) == startChar;
+    }
+
+
+
+
+        //
                
     }
     
